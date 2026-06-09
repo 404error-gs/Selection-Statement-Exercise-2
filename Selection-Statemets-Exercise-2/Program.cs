ddb9f34a171f6f138ac0e78bd8fc4edb146bc109
404error-gs <gensi1955@gmail.com>
2026-06-09 10:13:15 -0500

    commit changes

--- Selection-Statemets-Exercise-2/Program.cs
+++ Selection-Statemets-Exercise-2/Program.cs
@@ -49,7 +49,7 @@ class Program
 
             default:
                 Console.WriteLine("Interesting, Bet you're good at it!");
-                break;
+                break; 
         }
 
     }
